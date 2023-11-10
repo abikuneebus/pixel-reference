3c48471db63b7e9f117a9dcd1b41a16fbc923912
Brennan <113397554+abikuneebus@users.noreply.github.com>
2023-11-10 15:48:51 -0500

    implemented ability to select a shape, and rotate it with  '+'/'-' buttons

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -8,9 +8,13 @@ interface ControlPanelProps {
     height: number,
     shape: "rectangle" | "circle" | "triangle"
   ) => void;
+  onRotate: (angleDelta: number) => void;
 }
 
-const ControlPanel: React.FC<ControlPanelProps> = ({ onGenerate }) => {
+const ControlPanel: React.FC<ControlPanelProps> = ({
+  onGenerate,
+  onRotate,
+}) => {
   // initialize dimensions to 0
   const [width, setWidth] = useState<number>(0);
   const [height, setHeight] = useState<number>(0);
@@ -25,38 +29,44 @@ const ControlPanel: React.FC<ControlPanelProps> = ({ onGenerate }) => {
 
   // 'onSubmit' calls 'handleSubmit', which calls 'onGenerate' with current state values
   return (
-    <div className="control-panel">
-    <form onSubmit={handleSubmit}>
-      <select
-        value={shape}
-        onChange={(e) =>
-          setShape(e.target.value as "rectangle" | "circle" | "triangle")
-        }
-      >
-        <option value='rectangle'>Rectangle</option>
-        <option value='circle'>Circle</option>
-        {/* <option value='triangle'>Triangle</option> */}
-      </select>
-      <label>
-        W (px):
-        {/* 'onChange' handler updates 'width' state with new values */}
-        <input
-          type='number'
-          value={width}
-          onChange={(e) => setWidth(Number(e.target.value))}
-        />
-      </label>
-      <label>
-        H (px):
-        {/* 'onChange' handler updates 'height' state with new values */}
-        <input
-          type='number'
-          value={height}
-          onChange={(e) => setHeight(Number(e.target.value))}
-        />
-      </label>
-      <button type='submit'>Generate</button>
-    </form>
+    <div className='control-panel'>
+      <div className='generation'>
+        <form onSubmit={handleSubmit}>
+          <select
+            value={shape}
+            onChange={(e) =>
+              setShape(e.target.value as "rectangle" | "circle" | "triangle")
+            }
+          >
+            <option value='rectangle'>Rectangle</option>
+            <option value='circle'>Circle</option>
+            {/* <option value='triangle'>Triangle</option> */}
+          </select>
+          <label>
+            W (px):
+            {/* 'onChange' handler updates 'width' state with new values */}
+            <input
+              type='number'
+              value={width}
+              onChange={(e) => setWidth(Number(e.target.value))}
+            />
+          </label>
+          <label>
+            H (px):
+            {/* 'onChange' handler updates 'height' state with new values */}
+            <input
+              type='number'
+              value={height}
+              onChange={(e) => setHeight(Number(e.target.value))}
+            />
+          </label>
+          <button type='submit'>Generate</button>
+        </form>
+      </div>
+      <div className='adjustment'>
+        <button onClick={() => onRotate(1)}>+</button>
+        <button onClick={() => onRotate(-1)}>-</button>
+      </div>
     </div>
   );
 };
